dcd90f9c2804ce518ba7df9dd408972058effd44
shoaib-khan895 <71784547+shoaib-khan895@users.noreply.github.com>
2022-11-07 11:00:09 +0530

    local

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async ()=>{
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true
+    };
+  }
+});
 
 export default function App() {
+  function scheduleNotificaionHandler() {
+    Notifications.scheduleNotificationAsync({
+      content:{
+        title:'Local Notfication title',
+        body:'local notification body',
+        data:{userName :'Max'}
+      },
+      trigger:{
+        seconds:5
+      }
+    });
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificaionHandler}
+      ></Button>
     </View>
   );
 }
@@ -13,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
